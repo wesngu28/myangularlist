ad3c60f6a898a51b089c557ee598449f500caafd
wesngu28 <wesngu028@gmail.com>
2023-05-02 09:18:22 -0700

    add hero component and styles

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent {
+  @Input()
+  color!: string;
+  @Input()
+  text!: string;
+  @Output()
+  clickEvent = new EventEmitter()
+
+  onClick() { this.clickEvent.emit() }
+}
