d85bc1d77ab258691436f634426083e9355d9670
Wesley <60171023+wesngu28@users.noreply.github.com>
2023-05-03 13:41:07 -0700

    return a response

--- backend/api/list.ts
+++ backend/api/list.ts
@@ -19,18 +19,18 @@ export default async function parser (req: Request): Promise<Array<rating> | {er
         const shows = table.rawAttrs.replace(`class="list-table" data-items="`, '')
         let list = shows.slice(0, shows.length-2).replaceAll("&quot;", `"`)
         const databroadcasts = list.indexOf("data-broadcasts")
-        if (databroadcasts === -1) return { error: "Invalid parameters provided"}
+        if (databroadcasts === -1) return new Response({ error: "Invalid parameters provided"})
         const showsObject = databroadcasts > -1 ? JSON.parse(list.slice(0, databroadcasts-2)) : JSON.parse(list + "]")
         return showsObject.map((show: { score: any; anime_title: any; manga_title: any; anime_title_eng: any; manga_english: any; anime_url: any; manga_url: any; anime_image_path: any; manga_image_path: any }) => {
-            return {
+            return new Response({
                 rating: show.score,
                 title: listtype === "anime" ? show.anime_title : show.manga_title,
                 title_eng: listtype === "anime" ? show.anime_title_eng : show.manga_english,
                 url: listtype === "anime" ? show.anime_url : show.manga_url,
                 img: listtype === "anime" ? show.anime_image_path : show.manga_image_path
-            }
+            })
         });
     } else {
-        return { error: "Invalid parameters provided"}
+        return new Response({ error: "Invalid parameters provided"})
     }
 }
