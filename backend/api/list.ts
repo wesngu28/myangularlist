ae350672c03d980c1fd26b150eb964e2e0addc62
Wesley <60171023+wesngu28@users.noreply.github.com>
2023-05-03 13:58:45 -0700

    test error catching

--- backend/api/list.ts
+++ backend/api/list.ts
@@ -14,28 +14,33 @@ export default async function parser (req: Request): Promise<Array<rating> | {er
     const malhtml = await fetch(link)
     const maljson = await malhtml.text()
     const uncleanlist = parse(maljson)
-    let table = uncleanlist.querySelector('table')
-    if (table) {
-        const shows = table.rawAttrs.replace(`class="list-table" data-items="`, '')
-        let list = shows.slice(0, shows.length-2).replaceAll("&quot;", `"`)
-        const databroadcasts = list.indexOf("data-broadcasts")
-        const showsObject = databroadcasts > -1 ? JSON.parse(list.slice(0, databroadcasts-2)) : JSON.parse(list + "]")
-        return showsObject.map((show: { score: any; anime_title: any; manga_title: any; anime_title_eng: any; manga_english: any; anime_url: any; manga_url: any; anime_image_path: any; manga_image_path: any }) => {
-            return new Response(JSON.stringify({
-                rating: show.score,
-                title: listtype === "anime" ? show.anime_title : show.manga_title,
-                title_eng: listtype === "anime" ? show.anime_title_eng : show.manga_english,
-                url: listtype === "anime" ? show.anime_url : show.manga_url,
-                img: listtype === "anime" ? show.anime_image_path : show.manga_image_path
-            }), 
-            {
-                status: 200,
-                headers: {
-                    'content-type': 'application/json',
-                },
-            })
-        });
-    } else {
+    try {
+        let table = uncleanlist.querySelector('table')
+        if (table) {
+            const shows = table.rawAttrs.replace(`class="list-table" data-items="`, '')
+            let list = shows.slice(0, shows.length-2).replaceAll("&quot;", `"`)
+            const databroadcasts = list.indexOf("data-broadcasts")
+            const showsObject = databroadcasts > -1 ? JSON.parse(list.slice(0, databroadcasts-2)) : JSON.parse(list + "]")
+            return showsObject.map((show: { score: any; anime_title: any; manga_title: any; anime_title_eng: any; manga_english: any; anime_url: any; manga_url: any; anime_image_path: any; manga_image_path: any }) => {
+                return new Response(JSON.stringify({
+                    rating: show.score,
+                    title: listtype === "anime" ? show.anime_title : show.manga_title,
+                    title_eng: listtype === "anime" ? show.anime_title_eng : show.manga_english,
+                    url: listtype === "anime" ? show.anime_url : show.manga_url,
+                    img: listtype === "anime" ? show.anime_image_path : show.manga_image_path
+                }), 
+                {
+                    status: 200,
+                    headers: {
+                        'content-type': 'application/json',
+                    },
+                })
+            });
+        } else {
+            throw new Error("No table exists")
+        }
+    } catch (err) {
+        console.log(err)
         return new Response(
             JSON.stringify({ error: "Invalid parameters provided"}),
             {
