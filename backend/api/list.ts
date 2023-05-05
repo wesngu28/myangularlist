5ce3f06f16a58cbf14824d7ab95c21c024d04fa8
wesngu28 <wesngu028@gmail.com>
2023-05-04 22:33:51 -0700

    return scores as part of the api

--- backend/api/list.ts
+++ backend/api/list.ts
@@ -19,13 +19,16 @@ export default async function parser (req: Request) {
             let list = shows.slice(0, shows.length-2).replaceAll("&quot;", `"`)
             const databroadcasts = list.indexOf("data-broadcasts")
             const showsObject = databroadcasts > -1 ? JSON.parse(list.slice(0, databroadcasts-2)) : JSON.parse(list + "]")
-            return new Response(JSON.stringify(showsObject.map((show: { score: any; anime_title: any; manga_title: any; anime_title_eng: any; manga_english: any; anime_url: any; manga_url: any; anime_image_path: any; manga_image_path: any }) => {
+            return new Response(JSON.stringify(showsObject.map((show: any) => {
                 return {
                     rating: show.score,
+                    tier: show.score < 3 ? "F" : show.score < 5 ? "D" : show.score < 7  ? "C" : show.score < 7 ? "B" : show.score < 10 ? "A" : "S",
                     title: listtype === "anime" ? show.anime_title : show.manga_title,
                     title_eng: listtype === "anime" ? show.anime_title_eng : show.manga_english,
                     url: listtype === "anime" ? show.anime_url : show.manga_url,
-                    img: listtype === "anime" ? show.anime_image_path : show.manga_image_path
+                    img: listtype === "anime" ? show.anime_image_path : show.manga_image_path,
+                    release_date: listtype === "anime" ? show.anime_start_date_string : show.manage_start_date_string,
+                    created_at: show.created_at
                 }
             })),
             {
