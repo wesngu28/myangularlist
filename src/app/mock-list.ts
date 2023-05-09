ebf609d101719a7ac086a3aa9bf5b565cea22f5b
wesngu28 <wesngu028@gmail.com>
2023-05-09 15:45:41 -0700

    feat: add mock list for local development

---
+++ src/app/mock-list.ts
@@ -0,0 +1,422 @@
+export const list = [
+    {
+      "rating": 4,
+      "tier": "D",
+      "title": "Akame ga Kill!",
+      "title_eng": "Akame ga Kill!",
+      "url": "/anime/22199/Akame_ga_Kill",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1429/95946.jpg?s=7faffc350d3fdfc43bd01d216eff0981",
+      "release_date": "07-07-14",
+      "created_at": 1474771387
+    },
+    {
+      "rating": 9,
+      "tier": "A",
+      "title": "Angel Beats!",
+      "title_eng": "Angel Beats!",
+      "url": "/anime/6547/Angel_Beats",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1244/111115.jpg?s=c0caff58282618d69acc370cf98c01a0",
+      "release_date": "04-03-10",
+      "created_at": 1474770714
+    },
+    {
+      "rating": 8,
+      "tier": "A",
+      "title": "Arknights: Reimei Zensou",
+      "title_eng": "Arknights Animation: Prelude to Dawn",
+      "url": "/anime/50205/Arknights__Reimei_Zensou",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1644/128406.jpg?s=6a92f86cf0fc3376ffb1052eb1f827f6",
+      "release_date": "10-29-22",
+      "created_at": 1680381385
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Blend S",
+      "title_eng": "BLEND-S",
+      "url": "/anime/34618/Blend_S",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/6/88286.jpg?s=433abf38926f5ca3fe58aa29fd7cc093",
+      "release_date": "10-08-17",
+      "created_at": 1525064898
+    },
+    {
+      "rating": 10,
+      "tier": "S",
+      "title": "Bocchi the Rock!",
+      "title_eng": "Bocchi the Rock!",
+      "url": "/anime/47917/Bocchi_the_Rock",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1448/127956.jpg?s=18b1224e7a7b47317fdc79504b5e9c71",
+      "release_date": "10-09-22",
+      "created_at": 1679414216
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Cyberpunk: Edgerunners",
+      "title_eng": "",
+      "url": "/anime/42310/Cyberpunk__Edgerunners",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1818/126435.jpg?s=0be17183a1db49a9a2fb43affb404d81",
+      "release_date": "09-13-22",
+      "created_at": 1680242902
+    },
+    {
+      "rating": 5,
+      "tier": "C",
+      "title": "Darling in the FranXX",
+      "title_eng": "DARLING in the FRANXX",
+      "url": "/anime/35849/Darling_in_the_FranXX",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1614/90408.jpg?s=006ec3c5fe1915e09d2fed51a1591d12",
+      "release_date": "01-13-18",
+      "created_at": 1523332057
+    },
+    {
+      "rating": 10,
+      "tier": "S",
+      "title": "Girls &amp; Panzer",
+      "title_eng": "Girls und Panzer",
+      "url": "/anime/14131/Girls___Panzer",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/9/40969.jpg?s=f9fa8d7b1a0a956a9fa73a0a7989aa56",
+      "release_date": "10-09-12",
+      "created_at": 1581357203
+    },
+    {
+      "rating": 6,
+      "tier": "C",
+      "title": "High School DxD",
+      "title_eng": "High School DxD",
+      "url": "/anime/11617/High_School_DxD",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1331/111940.jpg?s=8bf33cff93cd27838bf307b3a8eeff1a",
+      "release_date": "01-06-12",
+      "created_at": 1474771543
+    },
+    {
+      "rating": 3,
+      "tier": "D",
+      "title": "Kaifuku Jutsushi no Yarinaoshi",
+      "title_eng": "Redo of Healer",
+      "url": "/anime/40750/Kaifuku_Jutsushi_no_Yarinaoshi",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1301/110018.jpg?s=c338fab9054ef91b77f5a414184b4cae",
+      "release_date": "01-13-21",
+      "created_at": 1617313311
+    },
+    {
+      "rating": 6,
+      "tier": "C",
+      "title": "Kantai Collection: KanColle",
+      "title_eng": "KanColle: Kantai Collection",
+      "url": "/anime/21511/Kantai_Collection__KanColle",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/4/73954.jpg?s=6fe7f28faf495a51be5d54593e0b00e1",
+      "release_date": "01-08-15",
+      "created_at": 1581357438
+    },
+    {
+      "rating": 9,
+      "tier": "A",
+      "title": "Koe no Katachi",
+      "title_eng": "A Silent Voice",
+      "url": "/anime/28851/Koe_no_Katachi",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1122/96435.jpg?s=93a12868f53676988803e1d1cd22f1d2",
+      "release_date": "09-17-16",
+      "created_at": 1680203119
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Kuma Kuma Kuma Bear",
+      "title_eng": "Kuma Kuma Kuma Bear",
+      "url": "/anime/40974/Kuma_Kuma_Kuma_Bear",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1413/110712.jpg?s=ae0db7448de75ce6c9d0413fcd3089a9",
+      "release_date": "10-07-20",
+      "created_at": 1679989492
+    },
+    {
+      "rating": 8,
+      "tier": "A",
+      "title": "Majo no Tabitabi",
+      "title_eng": "Wandering Witch: The Journey of Elaina",
+      "url": "/anime/40571/Majo_no_Tabitabi",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1802/108501.jpg?s=5eab9cb4a7e5f51cf9ef91f9d3127a6f",
+      "release_date": "10-02-20",
+      "created_at": 1617474144
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Nanatsu no Taizai",
+      "title_eng": "The Seven Deadly Sins",
+      "url": "/anime/23755/Nanatsu_no_Taizai",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/8/65409.jpg?s=46c7072f51a0af3032faa65c3dc63fdf",
+      "release_date": "10-05-14",
+      "created_at": 1523328814
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Nanatsu no Taizai OVA",
+      "title_eng": "The Seven Deadly Sins: Ban&#039;s Side Story OVA",
+      "url": "/anime/30347/Nanatsu_no_Taizai_OVA",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1973/97505.jpg?s=1c0430a02496c01bd4e96907d9e7c13d",
+      "release_date": "06-17-15",
+      "created_at": 1523328835
+    },
+    {
+      "rating": 0,
+      "tier": "F",
+      "title": "Nanatsu no Taizai: Imashime no Fukkatsu",
+      "title_eng": "The Seven Deadly Sins: Revival of the Commandments",
+      "url": "/anime/34577/Nanatsu_no_Taizai__Imashime_no_Fukkatsu",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/11/90089.jpg?s=5b75c2d494d206554520a34f4b2a34b2",
+      "release_date": "01-13-18",
+      "created_at": 1523328840
+    },
+    {
+      "rating": 6,
+      "tier": "C",
+      "title": "Nanatsu no Taizai: Seisen no Shirushi",
+      "title_eng": "The Seven Deadly Sins: Signs of Holy War",
+      "url": "/anime/31722/Nanatsu_no_Taizai__Seisen_no_Shirushi",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/13/79331.jpg?s=1a27c3d9bc318fa843465ec4aa64a47f",
+      "release_date": "08-28-16",
+      "created_at": 1523328829
+    },
+    {
+      "rating": 8,
+      "tier": "A",
+      "title": "No Game No Life",
+      "title_eng": "No Game, No Life",
+      "url": "/anime/19815/No_Game_No_Life",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1074/111944.jpg?s=47dc6b0ee368ca7e7894f0ab592582b4",
+      "release_date": "04-09-14",
+      "created_at": 1474772065
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Pokemon Generations",
+      "title_eng": "Pokémon Generations",
+      "url": "/anime/34514/Pokemon_Generations",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/11/83210.jpg?s=65b58f95d797f6e348c250e7f0335e89",
+      "release_date": "12-09-16",
+      "created_at": 1679618483
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Princess Connect! Re:Dive",
+      "title_eng": "",
+      "url": "/anime/39292/Princess_Connect_Re_Dive",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1810/106070.jpg?s=764b470ecb39212db6851112071b1014",
+      "release_date": "04-07-20",
+      "created_at": 1617770138
+    },
+    {
+      "rating": 8,
+      "tier": "A",
+      "title": "Re:Zero kara Hajimeru Isekai Seikatsu",
+      "title_eng": "Re:ZERO -Starting Life in Another World-",
+      "url": "/anime/31240/Re_Zero_kara_Hajimeru_Isekai_Seikatsu",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1522/128039.jpg?s=fa18667f7e01c40f0c08efed87bd8a4b",
+      "release_date": "04-04-16",
+      "created_at": 1474770585
+    },
+    {
+      "rating": 9,
+      "tier": "A",
+      "title": "Shokugeki no Souma",
+      "title_eng": "Food Wars! Shokugeki no Soma",
+      "url": "/anime/28171/Shokugeki_no_Souma",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/3/72943.jpg?s=04c9e4b3b83fc51f312bdb6fc0e74bff",
+      "release_date": "04-04-15",
+      "created_at": 1525063975
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Shokugeki no Souma OVA",
+      "title_eng": "",
+      "url": "/anime/31327/Shokugeki_no_Souma_OVA",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1622/96638.jpg?s=62a9d08ec59d5bb06598d4117b22f0c1",
+      "release_date": "05-02-16",
+      "created_at": 1525063992
+    },
+    {
+      "rating": 9,
+      "tier": "A",
+      "title": "Shokugeki no Souma: Ni no Sara",
+      "title_eng": "Food Wars! The Second Plate",
+      "url": "/anime/32282/Shokugeki_no_Souma__Ni_no_Sara",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/8/79353.jpg?s=9a56d3ce84f4b88008d059af4f30d7f3",
+      "release_date": "07-02-16",
+      "created_at": 1525064006
+    },
+    {
+      "rating": 8,
+      "tier": "A",
+      "title": "Shokugeki no Souma: Ni no Sara OVA",
+      "title_eng": "",
+      "url": "/anime/34480/Shokugeki_no_Souma__Ni_no_Sara_OVA",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1032/96640.jpg?s=fa66dcc8c4a2b82ffe32254616382229",
+      "release_date": "05-01-17",
+      "created_at": 1525064064
+    },
+    {
+      "rating": 8,
+      "tier": "A",
+      "title": "Shokugeki no Souma: San no Sara",
+      "title_eng": "Food Wars! The Third Plate",
+      "url": "/anime/35788/Shokugeki_no_Souma__San_no_Sara",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/3/88434.jpg?s=ac40ec67435f30585ad164f586ccd43b",
+      "release_date": "10-04-17",
+      "created_at": 1525064026
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Slime Taoshite 300-nen, Shiranai Uchi ni Level Max ni Nattemashita",
+      "title_eng": "I&#039;ve Been Killing Slimes for 300 Years and Maxed Out My Level",
+      "url": "/anime/40586/Slime_Taoshite_300-nen_Shiranai_Uchi_ni_Level_Max_ni_Nattemashita",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1888/133089.jpg?s=8989e341314f266775d683d57e8cd7db",
+      "release_date": "04-10-21",
+      "created_at": 1679633557
+    },
+    {
+      "rating": 9,
+      "tier": "A",
+      "title": "Sono Bisque Doll wa Koi wo Suru",
+      "title_eng": "My Dress-Up Darling",
+      "url": "/anime/48736/Sono_Bisque_Doll_wa_Koi_wo_Suru",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1179/119897.jpg?s=947c4f14aa219793bf7cbc170a6ad951",
+      "release_date": "01-09-22",
+      "created_at": 1681440413
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "SSSS.Dynazenon",
+      "title_eng": "SSSS.Dynazenon",
+      "url": "/anime/40870/SSSSDynazenon",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1880/113766.jpg?s=757ab46a9cd078b2b7176c4e020a163b",
+      "release_date": "04-02-21",
+      "created_at": 1638162655
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "SSSS.Gridman",
+      "title_eng": "SSSS.Gridman",
+      "url": "/anime/35847/SSSSGridman",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1973/95616.jpg?s=da21aeda1277a0eb5aba2f76cc610163",
+      "release_date": "10-07-18",
+      "created_at": 1638072102
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Suzume no Tojimari",
+      "title_eng": "Suzume",
+      "url": "/anime/50594/Suzume_no_Tojimari",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1598/128450.jpg?s=aedb2aa747318b2a07d797d3b29e3301",
+      "release_date": "11-11-22",
+      "created_at": 1681796917
+    },
+    {
+      "rating": 6,
+      "tier": "C",
+      "title": "Sword Art Online",
+      "title_eng": "Sword Art Online",
+      "url": "/anime/11757/Sword_Art_Online",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/11/39717.jpg?s=55f88992df908bdcb872db3a398aea49",
+      "release_date": "07-08-12",
+      "created_at": 1474771379
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Sword Art Online II",
+      "title_eng": "Sword Art Online II",
+      "url": "/anime/21881/Sword_Art_Online_II",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1223/121999.jpg?s=7acaaea9cca4b77091994a11393346ed",
+      "release_date": "07-05-14",
+      "created_at": 1474772087
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Trinity Seven",
+      "title_eng": "Trinity Seven",
+      "url": "/anime/25157/Trinity_Seven",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/12/67795.jpg?s=420feb1669ee030ca48bef01da997091",
+      "release_date": "10-08-14",
+      "created_at": 1474772097
+    },
+    {
+      "rating": 10,
+      "tier": "S",
+      "title": "Yuru Camp△",
+      "title_eng": "Laid-Back Camp",
+      "url": "/anime/34798/Yuru_Camp△",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/4/89877.jpg?s=21eb8033b346386d7adb9802a37e8829",
+      "release_date": "01-04-18",
+      "created_at": 1679508781
+    },
+    {
+      "rating": 10,
+      "tier": "S",
+      "title": "Yuru Camp△ Movie",
+      "title_eng": "Laid-Back Camp: The Movie",
+      "url": "/anime/38475/Yuru_Camp△_Movie",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1052/123863.jpg?s=cef474a9d865684f20b00c5faf9729f8",
+      "release_date": "07-01-22",
+      "created_at": 1679557131
+    },
+    {
+      "rating": 10,
+      "tier": "S",
+      "title": "Yuru Camp△ Season 2",
+      "title_eng": "Laid-Back Camp Season 2",
+      "url": "/anime/38474/Yuru_Camp△_Season_2",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/1255/110636.jpg?s=7b2762cd4e39f3b975d9efa53e96a642",
+      "release_date": "01-07-21",
+      "created_at": 1679548807
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Zero no Tsukaima",
+      "title_eng": "The Familiar of Zero",
+      "url": "/anime/1195/Zero_no_Tsukaima",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/8/20680.jpg?s=81fff271be02055eef31b1994d40cfa5",
+      "release_date": "07-03-06",
+      "created_at": 1474773319
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Zero no Tsukaima F",
+      "title_eng": "The Familiar of Zero F",
+      "url": "/anime/11319/Zero_no_Tsukaima_F",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/2/75559.jpg?s=64205031506bfe5effa675e139a9887c",
+      "release_date": "01-07-12",
+      "created_at": 1474773359
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Zero no Tsukaima: Futatsuki no Kishi",
+      "title_eng": "The Familiar of Zero: Knight of the Twin Moons",
+      "url": "/anime/1840/Zero_no_Tsukaima__Futatsuki_no_Kishi",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/2/22740.jpg?s=bdd23f5d71bdef0962049fe11fba217e",
+      "release_date": "07-09-07",
+      "created_at": 1474773333
+    },
+    {
+      "rating": 7,
+      "tier": "A",
+      "title": "Zero no Tsukaima: Princesses no Rondo",
+      "title_eng": "The Familiar of Zero: Rondo of Princesses",
+      "url": "/anime/3712/Zero_no_Tsukaima__Princesses_no_Rondo",
+      "img": "https://cdn.myanimelist.net/r/192x272/images/anime/6/10256.jpg?s=5a255c734d55bd629584818327e60cc0",
+      "release_date": "07-07-08",
+      "created_at": 1474773343
+    }
+  ]
--- src/app/services/mal-parser.service.ts
+++ src/app/services/mal-parser.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { list } from '../mock-list';
 
 interface rating {rating: string, title: string, title_eng: string, url: string, img: string}
 
@@ -11,9 +12,10 @@ export class MalParserService {
   constructor() { }
 
   async malparse(username: string, listtype: string) {
-    const link = `https://maledge.vercel.app/api/list?user=${username}&type=${listtype}`;
-    const malhtml = await fetch(link)
-    const maltext = await malhtml.json()
+    const maltext = list
+    // const link = `https://maledge.vercel.app/api/list?user=${username}&type=${listtype}`;
+    // const malhtml = await fetch(link)
+    // const maltext = await malhtml.json()
     return new Observable(observer => {
       observer.next(maltext);
       observer.complete();
