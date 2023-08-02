4f001d7c6f4a0027837bbf587aff00ee818e3bf5
wesngu28 <wesley@wesngu28.com>
2023-08-01 23:28:43 -0700

    feat: continue modal component building

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -9,15 +9,25 @@ import { ModalService } from 'src/app/services/modal.service';
 export class ModalComponent {
   showData: any;
   showModal: boolean = false;
+  malLink: string = "";
+  image: string = "";
 
   constructor(public modalService: ModalService) {
     this.modalService.showModal$.subscribe((showModal: boolean) => {
       this.showModal = showModal;
-      this.showData = this.modalService.showData;
+      this.showData = JSON.parse(this.modalService.showData);
+      this.malLink = `https://myanimelist.net${this.showData.url}`
+      this.image = this.showData.img;
     });
   }
 
   closeModal() {
     this.modalService.closeModal()
+  }
+
+  @HostListener('document:click', ['$event'])
+  onModalClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (target.id === "modal") this.showModal = false
   }
 }
